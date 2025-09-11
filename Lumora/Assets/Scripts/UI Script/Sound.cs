455edf08a711035c8da9b00c9f0fd945eeb016cb
cass cochrane <150483589+cass0023@users.noreply.github.com>
2025-09-11 14:53:03 -0400

    Add audiomanager and controller

---
+++ Lumora/Assets/Scripts/UI Script/Sound.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class Sound
+{
+    public string name;
+    public AudioClip clip;
+}
---
+++ Lumora/Assets/Scripts/UI Script/UIAudioController.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    //UI interactions with Audio Manager
+    //Attached to options canvas so will only be active when in canvas
+    public Slider musicSlider, sfxSlider;
+    void Awake()
+    {
+
+    }
+
+    //button press to mute/unmute audio
+    public void ToggleMusic()
+    {
+        AudioManager.Instance.ToggleMusic();
+    }
+    public void ToggleSFX()
+    {
+        AudioManager.Instance.ToggleSFX();
+    }
+
+    //Sets audio volume to value from slider
+    public void MusicVolume()
+    {
+        AudioManager.Instance.MusicVolume(musicSlider.value);
+    }
+    public void SFXVolume()
+    {
+        AudioManager.Instance.SFXVolume(sfxSlider.value);
+    }
+}
