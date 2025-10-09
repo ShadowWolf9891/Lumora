1b23bb3568c12c734ff97a72de011f70443997f3
cass cochrane <150483589+cass0023@users.noreply.github.com>
2025-10-09 19:19:22 -0400

    Add projectile prefab and script

---
+++ Lumora/Assets/Scripts/ProjectileBehaviour.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class ProjectileBehaviour : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            AudioManager.Instance.PlaySFX("");
+        }
+    }
+}
