d6316bfd79907d83c6568067b3cb27a9c0a61eb1
cass cochrane <150483589+cass0023@users.noreply.github.com>
2025-10-09 22:42:03 -0400

    Add SFX if collision with object asides ground layer

--- Lumora/Assets/Scripts/ProjectileBehaviour.cs
+++ Lumora/Assets/Scripts/ProjectileBehaviour.cs
@@ -2,24 +2,18 @@ using UnityEngine;
 
 public class ProjectileBehaviour : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
     void OnCollisionEnter(Collision collision)
     {
         //checks if collision with ground layer
-        if(collision.gameObject.layer == 3)
+        if (collision.gameObject.layer == 3)
         {
-            Debug.Log("Playing SFX");
             AudioManager.Instance.PlaySFX("ProjectileLanding");
+            Destroy(gameObject, 0.1f);
+        }
+        else
+        {
+            AudioManager.Instance.PlaySFX("ProjectileLanding");
+            Destroy(gameObject);
         }
     }
 }
