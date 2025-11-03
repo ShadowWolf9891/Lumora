3d0676733ed0b328ce1086d4408af8c85ce410b1
ShadowWolf9891 <cshort4403@gmail.com>
2025-11-03 15:54:29 -0500

    Add ability to spawn trigger

---
+++ Lumora/Assets/Scripts/Behaviors/SpawnableTriggerBehavior.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+[RequireComponent (typeof(BoxCollider))]
+public class SpawnableTriggerBehavior : MonoBehaviour
+{
+    private string EventToTrigger;
+	private int LayerMask;
+	private bool IsRepeatable;
+    private string Id;
+    public void Initialize(string id, string eventToTrigger, int layerMask, float radius, bool isRepeatable)
+    {
+        EventToTrigger = eventToTrigger;
+        LayerMask = layerMask;
+        IsRepeatable = isRepeatable;
+        Id = id;
+
+        BoxCollider collider = GetComponent<BoxCollider>();
+        collider.size = new Vector3(radius,radius,radius);
+        collider.includeLayers = LayerMask;
+    }
+
+	private void OnTriggerEnter(Collider other)
+	{
+        if(other.gameObject.layer == LayerMask) 
+        {
+            EventManager.Raise(EventToTrigger);
+            if(!IsRepeatable)
+            {
+                EventManager.MarkEventCompleted(Id);
+                Destroy(gameObject);
+            }
+        }
+	}
+}
