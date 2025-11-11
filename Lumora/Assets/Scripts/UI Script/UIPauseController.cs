78bb99204c05053b5d238addce23441207742be7
cass cochrane <150483589+cass0023@users.noreply.github.com>
2025-11-11 15:41:21 -0500

    Add button mapping for UI menus
    created a prefab of ui to work in separate scene

--- Lumora/Assets/Scripts/UI Script/UIPauseController.cs
+++ Lumora/Assets/Scripts/UI Script/UIPauseController.cs
@@ -2,18 +2,5 @@ using UnityEngine;
 
 public class UIPauseController : MonoBehaviour
 {
-    void Start()
-    {
-
-    }
-    public void ResumeGame()
-    {
-        this.gameObject.SetActive(false);
-    }
-    public void OpenOptions()
-    {
-        this.gameObject.SetActive(false);
-        UIManager.Instance.OptionsMenu();
     
-    }
 }
