3a4518534522629a7114cf0dc372d43a97a75360
ShadowWolf9891 <cshort4403@gmail.com>
2025-11-28 16:34:57 -0500

    Add mark as complete for timelines
    Timelines are now marked as complete when they end.

--- Lumora/Assets/Scripts/Managers/TimelineManager.cs
+++ Lumora/Assets/Scripts/Managers/TimelineManager.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -8,12 +9,17 @@ public static class TimelineManager
 {
 	public static List<PlayableDirector> _directors { get; private set; }
 
+	public static Dictionary<string, PlayableDirector> _eventTracker = new();
+
 	public static void Load()
 	{
 		//Might only load for first scene
 		_directors = GameObject.FindObjectsByType<PlayableDirector>(FindObjectsSortMode.InstanceID).ToList();
 		GameEvents<BeginCutsceneEvent>.Subscribe(StartTimeline);
+		foreach (var d in _directors)
+			d.stopped += OnDirectorStopped;
 	}
+
 
 	/// <summary>
 	/// Start a cutscene at a specific start time using the begin cutscene event. 
@@ -22,20 +28,33 @@ public static class TimelineManager
 	private static void StartTimeline(BeginCutsceneEvent e)
 	{
 		PlayableDirector director = _directors.Find(director => director.gameObject.name == e.TimelineName);
-		if (director == null)
+        if (director == null)
 		{
 			Debug.LogError($"Unable to find gameobject with name {e.TimelineName} in the scene. " +
 				$"Make sure you are calling Load() when the scene loads.");
 			return;
 		}
-
-		if(director.state != PlayState.Playing) 
+		if (!_eventTracker.ContainsKey(e.Id))
+		{
+			_eventTracker.Add(e.Id, director);
+		}
+		if (director.state != PlayState.Playing) 
 		{
 			director.time = e.StartTime;
 			director.Play();
 		}
 
 		//TODO: Implement End time to end the timeline when it reaches that point.
-
 	}
+	private static void OnDirectorStopped(PlayableDirector director)
+	{
+		foreach(var ev in _eventTracker) 
+		{
+			if(ev.Value == director) 
+			{
+				EventManager.MarkEventCompleted(ev.Key);
+			}
+		}
+	}
+
 }
