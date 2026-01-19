0a820b872d86169ff75c02e1ce63bdcfaebb1656
cass cochrane <150483589+cass0023@users.noreply.github.com>
2026-01-19 15:16:13 -0500

    Add button input to game over
    Controller : A
    Keyboard : Space

--- Lumora/Assets/Scripts/UI Script/UIGameOverPanel.cs
+++ Lumora/Assets/Scripts/UI Script/UIGameOverPanel.cs
@@ -1,13 +1,24 @@
 using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class UIGameOverPanel : MonoBehaviour
 {
+    private InputAction restart;
     [SerializeField]
     GameObject gameOverPanel;
     void Start()
     {
-        GameEvents<ChangeGameStateEvent>.Subscribe(OnGameStateChange);   
+        GameEvents<ChangeGameStateEvent>.Subscribe(OnGameStateChange);  
+        restart = InputSystem.actions.FindAction("South"); 
+    }
+    void Update()
+    {
+        if (restart.WasPressedThisFrame() && gameOverPanel.activeSelf)
+        {
+            SceneManager.LoadScene(0);
+        }
     }
 
     private void OnGameStateChange(ChangeGameStateEvent e)
