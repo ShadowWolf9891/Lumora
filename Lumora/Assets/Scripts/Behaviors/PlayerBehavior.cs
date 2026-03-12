44d2e63faa3a15f7b8e2c6b196fe05e7a319e91f
ShadowWolf9891 <cshort4403@gmail.com>
2026-03-12 10:00:31 -0400

    Fixed lots of warnings popping up
    The debug lines for the stealth system caused lots of warnings about colliders so they are now commented out.

--- Lumora/Assets/Scripts/Behaviors/PlayerBehavior.cs
+++ Lumora/Assets/Scripts/Behaviors/PlayerBehavior.cs
@@ -281,11 +281,11 @@ public class PlayerBehavior : MonoBehaviour, ISaveable
 
 		FaceMoveDirection(moveDirection);
 		//Debug for crouch movement, uncomment to re-enable.
-		Debug.DrawLine(transform.position, projectedNextPosition, UnityEngine.Color.green);
-		Debug.DrawLine(transform.position, currentCollider.ClosestPoint(transform.position), UnityEngine.Color.red);
-		Debug.DrawLine(nextPosition, currentCollider.ClosestPoint(nextPosition), UnityEngine.Color.red);
-		Debug.DrawLine(currentCollider.ClosestPoint(transform.position), currentCollider.ClosestPoint(nextPosition), UnityEngine.Color.orange);
-		Debug.DrawLine(transform.position, transform.position + wallNormal, UnityEngine.Color.blue);
+		//Debug.DrawLine(transform.position, projectedNextPosition, UnityEngine.Color.green);
+		//Debug.DrawLine(transform.position, currentCollider.ClosestPoint(transform.position), UnityEngine.Color.red);
+		//Debug.DrawLine(nextPosition, currentCollider.ClosestPoint(nextPosition), UnityEngine.Color.red);
+		//Debug.DrawLine(currentCollider.ClosestPoint(transform.position), currentCollider.ClosestPoint(nextPosition), UnityEngine.Color.orange);
+		//Debug.DrawLine(transform.position, transform.position + wallNormal, UnityEngine.Color.blue);
 	}
 
 	#endregion
