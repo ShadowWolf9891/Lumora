13a25b25c1d0d93e06f950542dec499bf9637d1b
cass cochrane <150483589+cass0023@users.noreply.github.com>
2026-04-04 23:07:53 -0400

    Move waypointcompass script to playerelement script

--- Lumora/Assets/Scripts/UI Script/UIPlayerCanvas.cs
+++ Lumora/Assets/Scripts/UI Script/UIPlayerCanvas.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class UIPlayerCanvas : MonoBehaviour
 {    
@@ -16,21 +18,39 @@ public class UIPlayerCanvas : MonoBehaviour
 
     public List<GameObject> currentUI = new List<GameObject>();
     public string currentLayer;
-    void Start()
+
+    
+    [Header("Waypoint Compass")]
+    private GameObject playerRef;
+    private GameObject cameraRef;
+    
+    //Waypoints
+    [SerializeField]private GameObject activeWaypoint;
+    [SerializeField]private Image waypointIcon;
+    
+    private TextMeshProUGUI iconText;
+    void Awake()
     {
         if (Instance == null)
 		{
 			Instance = this;
 		}
 		else Destroy(gameObject);
+
+        cameraRef = GameObject.Find("3rd Person Camera");
+        playerRef = GameObject.Find("Player");
+        activeWaypoint = playerRef.GetComponent<PlayerBehavior>().waypointImage;
+        iconText = GameObject.Find("T_Waypoint").GetComponentInChildren<TextMeshProUGUI>();
     }
     void OnEnable()
     {
         RefreshUI();
+        activeWaypoint = playerRef.GetComponent<PlayerBehavior>().waypointImage;
     }
     void OnDisable()
     {
         ClearUI();
+        activeWaypoint = null;
     }
     public void RefreshUI()
     {
@@ -73,7 +93,45 @@ public class UIPlayerCanvas : MonoBehaviour
     {
         currentLayer = layer.ToString();
     }
+
+    void Update()
+    {
+        UpdateCompass();
+    }
+    public void UpdateCompass()
+    {
+        Transform waypointTransform = activeWaypoint.transform;
+        Transform playerTransform = playerRef.transform;
+        Transform cameraTransform = cameraRef.transform;
+        
+        Vector3 direction = waypointTransform.position - playerTransform.position;
+        
+        direction.y = 0;
+
+        Vector3 camPos = cameraTransform.forward / 2;
+        camPos.y = 0;
+        
+        //Gets the horizontal angle between camera direction and waypoint
+        float angle = Vector3.SignedAngle(camPos, direction, Vector3.up);
+
+        //converts angle to UI pos
+        float compassWidth = 500f;
+        float normalized = angle / 60f;
+        float halfWidth = compassWidth / 2;
+
+        float xPos = normalized * halfWidth;
+        
+        float clampEnds = Mathf.Clamp(xPos, -halfWidth, halfWidth);
+        
+        
+        //moves waypoint icon and updates text
+        waypointIcon.rectTransform.anchoredPosition = new Vector2(clampEnds, waypointIcon.rectTransform.anchoredPosition.y);
+        
+        float distance = Vector3.Distance(playerTransform.position, waypointTransform.position);
+        iconText.text = Mathf.RoundToInt(distance) + "m";
+    }
 }
+
 [System.Serializable]
 public class TriggerControls
 {
